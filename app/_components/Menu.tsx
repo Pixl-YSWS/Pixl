b6f5439d79e1b5db66eb40cf67d3f6b549394120
Ridit <riditjangra09@gmail.com>
2026-05-31 19:24:19 +0530

    chore: add margin on test game button

--- app/_components/Menu.tsx
+++ app/_components/Menu.tsx
@@ -6,7 +6,7 @@ export function Menu() {
       </a>
       <a
         // onClick={handleRSVP}
-        className="text-center px-5 mr-2 py-2 text-base sm:text-xl md:text-2xl hover:shadow-2xl shadow-[#ff8c37] bg-[#ec3750] cursor-pointer text-white hover:-translate-y-1 hover:-translate-x-1 border-black border-r-8 border-t-2 border-l-2 hover:border-b-12 border-b-8 transition-all"
+        className="text-center px-5 mr-6 py-2 text-base sm:text-xl md:text-2xl hover:shadow-2xl shadow-[#ff8c37] bg-[#ec3750] cursor-pointer text-white hover:-translate-y-1 hover:-translate-x-1 border-black border-r-8 border-t-2 border-l-2 hover:border-b-12 border-b-8 transition-all"
         // whileHover={{ scale: 1.03 }}
         // whileTap={{ scale: 0.97 }}
         href="https://play.pixl.rsvp"
